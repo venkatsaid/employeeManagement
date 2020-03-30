4a779b5e84ed410595ab2c4965b79a385b925678
venkata sai Dasari <venkatsai9298@gmail.com>
2020-03-30 21:46:48 +0530

    Initial commit

---
+++ employeeapi/src/main/java/com/vs/employeeproj/employeeproj/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.vs.employeeproj.employeeproj.repository;
+
+import com.vs.employeeproj.employeeproj.model.Employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
---
+++ employeeapi/src/test/java/com/vs/employeeproj/employeeproj/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.employeeproj.employeeproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
